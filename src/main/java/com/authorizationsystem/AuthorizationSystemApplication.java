c908902f8e35a2819d70ec0c6b04348146550f15
Tiyasha Datta <tiyashadatta@gmail.com>
2023-06-10 23:20:58 +0530

    First commit

---
+++ src/main/java/com/authorizationsystem/AuthorizationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.authorizationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationSystemApplication.class, args);
+	}
+
+}
